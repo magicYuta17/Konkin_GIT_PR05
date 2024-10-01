75b1bcc08336e84870dd2e8b59f801d10e54750e
Студент251 <student251@zamt.local>
2024-10-01 09:44:56 +0300

    Добавление модуля Db.cs

---
+++ Db.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konkin_GIT_PR05
+{
+    class Db
+    {
+    }
+}
